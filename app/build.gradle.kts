eaffddf90ede1405c20c214e4bf85dc3b720fd2e
Jov Mit <jovmit@Jovs-MacBook-Pro-351.local>
2025-10-21 17:55:51 +0200

    Initial Project Setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose)
+}
+
+android {
+  namespace = "nl.jovmit.asynco"
+
+  compileSdk {
+    version = release(libs.versions.targetSdkVersion.get().toInt())
+  }
+
+  defaultConfig {
+    applicationId = "nl.jovmit.asynco"
+    minSdk = libs.versions.minSdkVersion.get().toInt()
+    targetSdk = libs.versions.targetSdkVersion.get().toInt()
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+
+  compileOptions {
+    val javaVersion = libs.versions.javaVersion.get()
+    sourceCompatibility = JavaVersion.toVersion(javaVersion)
+    targetCompatibility = JavaVersion.toVersion(javaVersion)
+  }
+
+  kotlin {
+    compilerOptions {
+      val javaVersion = libs.versions.javaVersion.get()
+      jvmTarget.set(JvmTarget.fromTarget(javaVersion))
+    }
+  }
+
+  buildFeatures {
+    compose = true
+  }
+}
+
+dependencies {
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.graphics)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.compose.material3)
+
+  debugImplementation(libs.androidx.compose.ui.tooling)
+  debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
+  testImplementation(libs.junit)
+}
